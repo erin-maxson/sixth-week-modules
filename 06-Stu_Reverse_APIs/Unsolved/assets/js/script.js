85f03f200af5f254f728a82b7808bed7857a15e4
erin-maxson <erin@cohatch.com>
2022-06-04 10:24:39 -0700

    more notes for student activity 6

--- 06-Stu_Reverse_APIs/Unsolved/assets/js/script.js
+++ 06-Stu_Reverse_APIs/Unsolved/assets/js/script.js
@@ -4,9 +4,14 @@ var requestUrl = 'https://api.github.com/orgs/Netflix/repos';
 // TODO: Comment on how AJAX returns an API call
 // requires the jQuery library connection
 // AJAX requests the API link and then logs the response in the console
+// conversion happens where the URL and method are
+// short term this will work, but long term you run the risk of the code breaking
 $.ajax({
+  // conversion happens here
   url: requestUrl,
   method: 'GET',
+
+  // final result
 }).then(function (response) {
   console.log('AJAX Response \n-------------');
   console.log(response);
@@ -15,6 +20,7 @@ $.ajax({
 // Browser Fetch Method
 // TODO: Comment on how Fetch returns an API call
 // Fetch grabs the API URL and returns the response in json format
+// fetch takes the data and stores it in the response, returns the data as .json, and then stores the data as .json in the data area
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
@@ -27,7 +33,7 @@ fetch(requestUrl)
 // Browser XMLHttpRequest
 // TODO: Comment on how XMLHttpRequest returns an API call
 // built in browser function, but requires more code to execute
-// XMLHttpRequest returns the API data as a string in the console
+// XMLHttpRequest returns the API data as a string in the console, if the data state is ready (200 request) it will return a string
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
   if (xhr.readyState === XMLHttpRequest.DONE) {
