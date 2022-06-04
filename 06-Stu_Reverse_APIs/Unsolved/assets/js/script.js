4dfefcacdeee063298c5a1b4f33cbff33265cccc
erin-maxson <erin@cohatch.com>
2022-06-04 10:11:40 -0700

    updating student activity 6

--- 06-Stu_Reverse_APIs/Unsolved/assets/js/script.js
+++ 06-Stu_Reverse_APIs/Unsolved/assets/js/script.js
@@ -2,6 +2,8 @@ var requestUrl = 'https://api.github.com/orgs/Netflix/repos';
 
 // JQuery AJAX
 // TODO: Comment on how AJAX returns an API call
+// requires the jQuery library connection
+// AJAX requests the API link and then logs the response in the console
 $.ajax({
   url: requestUrl,
   method: 'GET',
@@ -12,6 +14,7 @@ $.ajax({
 
 // Browser Fetch Method
 // TODO: Comment on how Fetch returns an API call
+// Fetch grabs the API URL and returns the response in json format
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
@@ -23,6 +26,8 @@ fetch(requestUrl)
 
 // Browser XMLHttpRequest
 // TODO: Comment on how XMLHttpRequest returns an API call
+// built in browser function, but requires more code to execute
+// XMLHttpRequest returns the API data as a string in the console
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
   if (xhr.readyState === XMLHttpRequest.DONE) {
@@ -34,3 +39,6 @@ xhr.open('GET', requestUrl);
 xhr.send();
 
 // TODO: Comment on the differences on the format of the data that was returned
+// AJAX = string in the console
+// fetch = json
+// XMLHttpRequest = string
