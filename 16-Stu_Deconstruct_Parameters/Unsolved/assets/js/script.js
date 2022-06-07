2c665ca3bf3a0885a9610121d37613d5491ae0a8
erin-maxson <erin@cohatch.com>
2022-06-06 17:49:54 -0700

    adding parameter notes to student activity 16

--- 16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
+++ 16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
@@ -1,5 +1,9 @@
 fetch(
   // Explain each parameter in comments below.
+  // per_page=10: allows 10 issue items only per page
+  // state=open means that the issue has to be open and not closed
+  // sort=created sorts the data by the creation date
+  // direction=desc' shows the data in the array in the order it was declared
   'https://api.github.com/repos/nodejs/node/issues?per_page=10&state=open&sort=created&direction=desc'
 )
   .then(function (response) {
