6b16da1df40773e8265c14ddd52a03d1b778b090
erin-maxson <erin@cohatch.com>
2022-06-06 17:37:37 -0700

    updating student activity 14

--- 14-Stu_Fetch_Status/Unsolved/assets/js/script.js
+++ 14-Stu_Fetch_Status/Unsolved/assets/js/script.js
@@ -10,7 +10,7 @@ function getApi(request) {
       console.log(response.status);
       // Then write the conditional based on that response.status value
       // Make sure to display the response on the page
-      if (response.status === 404) {
+      if (response.status != 200) {
         responseText.textContent = response.status;
       }
 
